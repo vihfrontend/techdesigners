89d41de7e08c077a82afd4c49da0c291a816463c
ViniciusDeveloper <125139361+vihfrontend@users.noreply.github.com>
2023-05-09 03:14:49 -0300

    atualizei

--- assest/scripts/menu.js
+++ assest/scripts/menu.js
@@ -2,6 +2,7 @@
     let menu = document.querySelector('.menu-hamburguer');
      let open = ()=>{
         let menucurrent = document.querySelector('.list-nav');
+        menu.classList.toggle('alternete')
         menucurrent.classList.toggle('open')
     }
     menu.addEventListener('click',open)
