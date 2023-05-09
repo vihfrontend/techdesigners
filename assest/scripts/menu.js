6eedcc90b96e1f80938ae3aa766dcf7e5cf48be6
ViniciusDeveloper <125139361+vihfrontend@users.noreply.github.com>
2023-05-09 02:04:39 -0300

    atualizei

--- assest/scripts/menu.js
+++ assest/scripts/menu.js
@@ -1,7 +1,7 @@
 (()=>{
-    let menu = document.querySelector('.hamburguer-active');
+    let menu = document.querySelector('.menu-hamburguer');
      let open = ()=>{
-        let menucurrent = document.querySelector('.main-itens');
+        let menucurrent = document.querySelector('.list-nav');
         menucurrent.classList.toggle('open')
     }
     menu.addEventListener('click',open)
