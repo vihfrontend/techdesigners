cac1e54455f671ddd973e9e467c38b597ad9b7de
ViniciusDeveloper <125139361+vihfrontend@users.noreply.github.com>
2023-05-10 04:48:04 -0300

    atualiza

---
+++ assest/scripts/reveal.js
@@ -0,0 +1,13 @@
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal('.text-hero', {
+    duration: 1000,
+    rotate: { y:70, x:0,z:0 }
+});
+sr.reveal('.card-services', {
+    duration: 1300,
+    rotate: { y:70, x:10,z:0 }
+});
+sr.reveal('.img-box', {
+    duration: 1500,
+});
