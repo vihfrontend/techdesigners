1bfb188b78899b13ac1cd4924133b577138cfba6
ViniciusDeveloper <125139361+vihfrontend@users.noreply.github.com>
2023-05-21 09:34:02 -0300

    atualizar

--- assest/portfolio/assest/css/script/menu.js
+++ assest/portfolio/assest/css/script/menu.js
@@ -7,5 +7,9 @@
         btn.classList.toggle('back');
     }
     btn.addEventListener('click',openmenu)
+    let anoatual = document.querySelector('#year');
+    let data = new Date();
+    let ano = '3000'
+    anoatual.innerHTML = ano
 
 })();
--- assest/scripts/menu.js
+++
@@ -1,9 +0,0 @@
-(()=>{
-    let menu = document.querySelector('.menu-hamburguer');
-     let open = ()=>{
-        let menucurrent = document.querySelector('.list-nav');
-        menu.classList.toggle('alternete')
-        menucurrent.classList.toggle('open')
-    }
-    menu.addEventListener('click',open)
-})();
