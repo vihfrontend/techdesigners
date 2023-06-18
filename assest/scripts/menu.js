98ed3dfbe07889b7b1e2a4d8f3307ebf23a75409
ViniciusDeveloper <125139361+vihfrontend@users.noreply.github.com>
2023-06-18 05:43:40 -0300

    atualizar

--- assest/scripts/menu.js
+++ assest/scripts/menu.js
@@ -2,12 +2,9 @@
 const btn = document.querySelector('.menu-hamburguer');
 const nav = document.querySelector('.list-nav');
 
-console.log(btn);
-console.log(nav);
 
 const openmenu = () => {
   nav.classList.toggle('open');
-  console.log(nav);
 };
 
 if (btn) {
